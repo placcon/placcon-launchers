94f8114eac94920ade6ffbaf8a636f53e6a6ba0f
Gergo Sofalvi <gergo.sofalvi@melonapp.hu>
2025-06-25 12:06:44 +0200

    Fix display api

--- main-order-display.js
+++ main-order-display.js
@@ -68,7 +68,7 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload-simple.js')
+      preload: path.join(__dirname, 'preload.js')
     },
     icon: path.join(__dirname, 'assets', 'icon.png'),
     title: 'placcon order display',
